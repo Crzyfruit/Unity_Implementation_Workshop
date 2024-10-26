bc517db0d78fd30e2967c276d65cce820d750fc0
Crzyfruit <114343660+Crzyfruit@users.noreply.github.com>
2024-10-26 12:38:12 +0200

    I am  giving up

--- Assets/Scenes/3. Music/AudioTrigger.cs
+++ Assets/Scenes/3. Music/AudioTrigger.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioTrigger : MonoBehaviour
 {
     //1. How to trigger an audio on start & using colliders
@@ -9,24 +11,63 @@ public class AudioTrigger : MonoBehaviour
 
     //3. Extra: how to implement audio into the animations
 
-
+    [SerializeField] private float fadeDuration = 1.0f;
     private AudioSource source;
-    private BoxCollider2D collider;
+    private Coroutine fadeCoroutine;
+
+    [Header("Trigger")]
+    [SerializeField] private bool OnEnter; 
+    [SerializeField] private bool OnExit; 
 
 
-    private void Awake()
+    private void Start()
     {
         source = GetComponent<AudioSource>();
-        collider = GetComponent<BoxCollider2D>();
+        source.volume = 0f;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        source.Play();
+        if (OnEnter)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (!source.isPlaying)
+                {
+                    source.Play();
+                    StartCoroutine(FadeAudio(source, 2f, 1f));
+                    Debug.Log("Enter!");
+                }
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        source.Stop();
+        if (OnExit)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (source.isPlaying)
+                {
+                    StartCoroutine(FadeAudio(source, 2f, 1f));
+                    source.Stop();
+                    Debug.Log("Exit!");
+                }
+            }
+        }
+    }
+
+    private IEnumerator FadeAudio(AudioSource source, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = source.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            source.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 }
